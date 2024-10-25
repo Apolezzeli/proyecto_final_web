6df6b86f35171376486d5293039e552bc7399fc8
apolezzeli <almendra.polezzeli@flexibility.com.ar>
2024-10-25 18:43:08 -0300

    config data send for form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
+const req = require('express/lib/request');
 var router = express.Router();
 
+var nodemailer = require('nodemailer');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var telefono = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  console.log(req.body)
+
+  var obj = {
+    to: 'apolezzeli@gmail.com',
+    subjet: 'Contacto desde la web',
+    html: nombre + " " + apellido + "<br> correo: " + email + "<br> tel: " + telefono + "<br> mensaje: " + mensaje,
+  }
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    }
+  })
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('index',{
+    message: "Mensaje enviado correctamente"
+  });
+}
+)
 
 module.exports = router;
--- routes/mas.js
+++ routes/mas.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET mas listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('mas'); // Renderizar la vista mas.hbs
 });
 
 module.exports = router;
