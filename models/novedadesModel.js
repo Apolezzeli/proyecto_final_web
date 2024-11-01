bb8cc044ffdf7b68123cbd02a88bacb42ad0bec7
apolezzeli <almendra.polezzeli@flexibility.com.ar>
2024-11-01 18:20:44 -0300

    finishing CRUD logic

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -1,9 +1,15 @@
 var pool = require('./bd');
 
 async function getNovedades() {
-    var query = "select * from novedades order by id desc";
+    var query = "select * from novedades order by id asc";
     var rows = await pool.query(query);
     return rows;
+}
+
+async function getNovedadById(id) {
+    var query = "select * from novedades where id=?";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
 }
 
 
@@ -14,8 +20,27 @@ async function deleteNovedadById(id) {
 
 }
 
+async function insertNovedad(obj) {
+    try {
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj]);
+        return rows;
 
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
 
+async function updateNovedadById(obj, id) {
+    try {
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
 
+    } catch (error) {
+        throw error;
+    }
+}
 
-module.exports = { getNovedades, deleteNovedadById }
+module.exports = { getNovedades, getNovedadById, deleteNovedadById, insertNovedad, updateNovedadById }
